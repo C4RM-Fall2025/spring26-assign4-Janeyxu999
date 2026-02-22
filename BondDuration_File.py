e0f6b5d443cfc1bc47b19f1b7216ce7e5d9dc226
Janeyxu999 <Janeyxu999@163.com>
2026-02-22 02:35:56 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,23 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    m_eff = m * ppy
+    couponRate = couponRate / ppy
+    y_eff = y / ppy
+
+    pvcfsum = 0
+    WeightedTimeSum = 0
+
+    for t in range(1, m_eff + 1):
+        pvm = (1+y_eff) ** (-t)
+        cf = face * couponRate_eff
+
+        if t == m_eff:
+            cf += face
+
+        pvcf = cf * pvm
+        pvcfsum += pvcf
+
+        WeightedTimeSum += pvcf * (t / ppy)
+
+    BondDuration = WeightedTimeSum / pvcfsum
+    return BondDuration
