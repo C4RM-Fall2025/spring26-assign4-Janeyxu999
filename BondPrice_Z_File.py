e2acc4d54a23762f707de90ac1d94a36b6593e6e
Janeyxu999 <Janeyxu999@163.com>
2026-02-22 12:29:47 -0500

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,17 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    
+    coupon = couponRate * face
+
+    bondPrice = 0
+
+    for t, y in zip(times, yc):
+        pvm = (1+y) ** (-t)
+        cf = coupon
+        if t == last_t:
+            cf += face
+        
+        bondPrice = cf * pvm
+
+    return bondPrice
