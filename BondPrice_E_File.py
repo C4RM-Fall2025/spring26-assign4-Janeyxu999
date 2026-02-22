b57512e7c88ebc4f813b0fe34a05ee057284afe9
Janeyxu999 <Janeyxu999@163.com>
2026-02-22 12:09:43 -0500

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,18 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    
+    coupon = couponRate * face
+    
+    bondPrice = 0
+
+    for t, y in enumerate(yc, start = 1):
+        pvm = (1+y) ** (-t)
+
+        cf = coupon
+        if t == len(yc)
+            cf += face
+
+        bondPrice += cf * pvm
+
+    return bondPrice
