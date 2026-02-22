7349678beeb13f7b37f93eb95e8026ba9c6d1cd9
Janeyxu999 <Janeyxu999@163.com>
2026-02-22 02:24:25 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,20 @@
 
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    m_eff = m * ppy
+    couponRate_eff = couponRate / ppy
+    y_eff = y / ppy
+
+    pvcfsum = 0
+    
+    for t in range(1, m_eff+1):
+        pvm = (1+y_eff) ** (-t)
+
+        cf = face * couponRate_eff
+        if t == m_eff:
+            cf += face
+
+        pvcfsum += cf * pvm
+    
+    return pvcfsum
+        
